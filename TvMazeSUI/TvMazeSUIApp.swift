9eda22daad88fbef1a1be278561cd6e085af722a
Andre Vinicius Torres Conrado <vinicius.torresc@gmail.com>
2024-03-25 18:18:09 -0300

    Initial Commit

---
+++ TvMazeSUI/TvMazeSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TvMazeSUIApp.swift
+//  TvMazeSUI
+//
+//  Created by Vini on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TvMazeSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
